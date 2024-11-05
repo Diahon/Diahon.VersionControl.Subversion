c0dde5c46d35d4af03104ff6236d818f18406b57
Lukas Kurz <lukas@shortdev.de>
2024-11-06 00:14:22 +0100

    Use test containers (#3)
    - Add test-containers
    - Move to xUnit v3
    - Fix freeze on invalid auth method (e.g. anonymous auth)

---
+++ Diahon.VersionControl.Subversion.Test/Env.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Diahon.VersionControl.Subversion.Test;
+
+static class Env
+{
+    public const string SvnRepoName = "repo2";
+    public static Uri RepoUrl { get; } = new($"svn://localhost/{SvnRepoName}");
+    public static NetworkCredential Credentials { get; } = new("some_user", "ps123");
+}
